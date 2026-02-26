75ff57595bddcdf3328b92f626b008320dc5a686
GABRIELA SILVA DA ROSA <04489635079@senacrs.edu.br>
2026-02-26 16:46:09 -0300

    refactor: modificada mensagem de login incorreto

--- login.js
+++ login.js
@@ -1,7 +1,11 @@
 function login (nome, senha){
     if(nome === "Leo" && senha === "123"){
         console.log("Seja bem-vindo!")
-    }else{
-        ("não foi")
     }
+    
+    else
+    {
+        ("Usuário ou senha incorretos! ")
+    }
+
 }
